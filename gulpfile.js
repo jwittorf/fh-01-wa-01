d01252894dbcfb6534a31f84ef0525e120928023
Julian Wittorf <jwittorf@die-netzwerkstatt.de>
2018-10-14 00:10:16 +0200

    [!] Refactor entire project, make it modern, see changes in this commit
    Good news first: the html and scss/stays 99% the same,
    it's even easier in the terminal, you just need `gulp` and `gulp production`
    
    * Expand .gitignore for exclusion of external and generated files
    * Drop fontawesome, use Twitter Bootstrap 3
    * .htaccess.sample for performance tweaks
    * Project now contains /src for development and /dist for production (minified code, etc.)
    ** Just open http://localhost/your-folder/src for development
    * Introduce gulp with npm/nodeJS, download and install here (no signup needed as far I know): https://nodejs.org/de/download/
    ** Tested with Node.js 6.11.3 and npm 3.10.10, bit outdated, I know, but it works, just give it a shot
    ** After installing Node.js and npm, run `npm install` from your terminal, it should install all dependencies and run the setup taks defined in gulpfile.js
    ** For further development just run `gulp` in your terminal
    ** For updating/creating the dist/ directory, just run `gulp production` in your terminal, you can always delete the entire dist/ directory, since it's generated when needed
    * I recommend you to exclude these directories from the project in PhpStorm
    ** dist
    ** src/css
    ** src/fonts

---
+++ gulpfile.js
@@ -0,0 +1,178 @@
+var gulp = require('gulp'),
+	merge_streams = require('merge-stream'),
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	htmlmin = require('gulp-htmlmin'),
+	uglify = require('gulp-uglify'),
+	pump = require('pump'),
+	imagemin = require('gulp-imagemin'),
+	rename = require('gulp-rename'),
+	sourcemaps = require('gulp-sourcemaps'),
+	livereload = require('gulp-livereload'),
+	filter = require('gulp-filter');
+
+/**
+ * Copy assets to dedicated directories
+ */
+gulp.task('install-assets', function () {
+
+	// Bootstrap Fonts
+	var fonts_twbs = gulp.src(
+		'node_modules/bootstrap-sass/assets/fonts/**/*',
+		{base: 'node_modules/bootstrap-sass/assets/fonts'})
+		.pipe(gulp.dest('src/fonts/vendor'));
+
+	// JavaScripts
+	var js = gulp.src([
+		'node_modules/jquery/dist/jquery.js',
+		'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
+		'node_modules/enquire.js/dist/enquire.min.js'
+	]).pipe(gulp.dest('src/js/vendor'));
+
+	return merge_streams(fonts_twbs, js);
+});
+
+
+/**
+ * Production assets
+ *
+ * Copy needed assets to production directory
+ */
+gulp.task('production-assets', function () {
+    // Fonts
+    var fonts = gulp.src('src/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts'));
+
+    // Stylesheets and JavaScripts are handled by minifier
+
+	var images = gulp.src('src/img/**/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('dist/img'));
+
+    var htaccess = gulp.src('.htaccess.sample')
+        .pipe(rename('.htaccess'))
+        .pipe(gulp.dest('dist'));
+
+	return merge_streams(fonts, images, htaccess);
+});
+
+/**
+ * Sass compilation for production
+ *
+ * Will compile sass into css with compressed with auto-prefixer
+ *
+ * @link https://github.com/dlmanning/gulp-sass
+ */
+gulp.task('sass:prod', function () {
+	return gulp.src('scss/**/*.scss')
+		.pipe(
+			sass({
+				outputStyle: 'compressed',
+				precision: 8,
+				indentType: 'tab',
+				indentWidth: 1
+			}).on('error', sass.logError)
+		)
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('dist/css/'));
+});
+
+
+/**
+ * JS minification for production
+ *
+ * Will create a min-version of all js files
+ *
+ * @link https://github.com/terinjokes/gulp-uglify
+ */
+gulp.task('jsminify', function (cb) {
+	pump([
+			gulp.src('src/js/**/*'),
+			uglify({
+				output: {
+					comments: true
+				}
+			}),
+			gulp.dest('dist/js')
+		],
+		cb
+	);
+});
+
+
+/**
+ * HTML minification for production
+ *
+ * Will strip spaces and comments from html
+ *
+ * @link https://github.com/jonschlinkert/gulp-htmlmin
+ */
+gulp.task('htmlminify', () => {
+    // Check if .shtml will work, too
+	return gulp.src('src/*.html')
+		.pipe(htmlmin({
+			minifyJS: true,
+			collapseWhitespace: true,
+			conservativeCollapse: true,
+			removeComments: true,
+			removeScriptTypeAttributes: true,
+			removeStyleLinkTypeAttributes: true
+		}))
+		.pipe(gulp.dest('dist'));
+});
+
+
+/**
+ * Sass compilation for development
+ *
+ * Will compile sass into css with sourcemaps and livereload
+ *
+ * @link https://github.com/dlmanning/gulp-sass
+ * @link https://github.com/floridoo/gulp-sourcemaps
+ * @link https://github.com/vohof/gulp-livereload
+ */
+gulp.task('sass:dev', function () {
+	return gulp.src('scss/**/*.scss')
+		.pipe(sourcemaps.init())
+		.pipe(
+			sass({
+				outputStyle: 'expanded',
+				precision: 8,
+				indentType: 'tab',
+				indentWidth: 1
+			}).on('error', sass.logError)
+		)
+		.pipe(sourcemaps.write('sourcemaps'))
+		.pipe(gulp.dest('src/css'))
+		// make sure livereload will only reload the css and not the entire page when passing .map files in
+		.pipe(filter('**/*.css'))
+		.pipe(livereload());
+});
+
+/**
+ * Common development task (default)
+ *
+ * This will run sass:dev before watching the sass files for changes
+ *
+ * @link https://github.com/vohof/gulp-livereload
+ */
+gulp.task('dev', ['sass:dev'], function () {
+	livereload.listen();
+	gulp.watch('scss/**/*.scss', ['sass:dev']);
+	gulp.watch([
+		'src/**/*.html',
+		'src/**/*.shtml',
+		'src/**/*.php',
+		'src/js/**/*',
+		'src/img/**/*'
+	], function (event) {
+		livereload.changed(event.path);
+	});
+});
+
+gulp.task('setup', ['install-assets', 'sass:dev']);
+gulp.task('default', ['dev']);
+gulp.task('production', ['sass:prod', 'jsminify', 'htmlminify', 'production-assets']);
