5ea27ded6eb9586040197f07abe672614f4adcf7
Julian Wittorf <jwittorf@die-netzwerkstatt.de>
2018-11-18 16:47:38 +0100

    [BUGFIX] Check if there is any data before anything else in form-result
    Otherwise will get errors of undefined indexes

--- src/form-result.php
+++ src/form-result.php
@@ -83,29 +83,25 @@
                         <div class="list-group">
 							<?php
 							// Collect all results, make sure to update this if new fields in index.shtml
-							$results = [
-								"Benutzername" => $_POST["username"],
-								"Passwort" => $_POST["password"],
-								"E-Mail" => $_POST["email"],
-								"Geburtsdatum" => $_POST["dob"]
-							];
-							// Prepare empty array for empty results
-							$emptyResults = [];
-							foreach ($results as $name => $input) {
-								if (!empty($input)) {
-									// Output every filled input with the $results key as heading
-									//(useful for multi-language websites with same html name attribute but different labels
-									echo "<div class=\"list-group-item\">";
-									echo "<h3 class=\"list-group-item-heading\">" . $name . "</h3>";
-									echo "<p class=\"list-group-item-text\">" . $input . "</p>";
-									echo "</div>";
-								} else {
-									// Collect empty inputs to compare with count of given input
-									array_push($emptyResults, $name);
+							if (!empty($_POST)) {
+								$results = [
+									"Benutzername" => $_POST["username"],
+									"Passwort" => $_POST["password"],
+									"E-Mail" => $_POST["email"],
+									"Geburtsdatum" => $_POST["dob"]
+								];
+								foreach ($results as $name => $input) {
+									if (!empty($input)) {
+										// Output every filled input with the $results key as heading
+										//(useful for multi-language websites with same html name attribute but different labels
+										echo "<div class=\"list-group-item\">";
+										echo "<h3 class=\"list-group-item-heading\">" . $name . "</h3>";
+										echo "<p class=\"list-group-item-text\">" . $input . "</p>";
+										echo "</div>";
+									}
 								}
-							}
-							if (count($emptyResults) === count($results)) {
-								// If, somehow, the html validation fails and the user submits empty values
+							} else {
+								// If the html validation fails and the user submits empty values (or file was accessed directly)
 								echo "<p class=\"alert alert-danger\">Achtung, alle Felder sind leer!</p>";
 							}
 							?>
