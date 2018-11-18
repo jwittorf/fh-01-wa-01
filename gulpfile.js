d55a912cdce535e88a7abadd88949ffe83593f22
Julian Wittorf <jwittorf@die-netzwerkstatt.de>
2018-11-18 16:31:55 +0100

    [TASK] Reindent/reformat gulp configuration file

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,35 @@
 var gulp = require('gulp'),
-	merge_streams = require('merge-stream'),
-	sass = require('gulp-sass'),
-	autoprefixer = require('gulp-autoprefixer'),
-	htmlmin = require('gulp-htmlmin'),
-	uglify = require('gulp-uglify'),
-	pump = require('pump'),
-	imagemin = require('gulp-imagemin'),
-	rename = require('gulp-rename'),
-	sourcemaps = require('gulp-sourcemaps'),
-	livereload = require('gulp-livereload'),
-	filter = require('gulp-filter');
+    merge_streams = require('merge-stream'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    htmlmin = require('gulp-htmlmin'),
+    uglify = require('gulp-uglify'),
+    pump = require('pump'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    sourcemaps = require('gulp-sourcemaps'),
+    livereload = require('gulp-livereload'),
+    filter = require('gulp-filter');
 
 /**
  * Copy assets to dedicated directories
  */
 gulp.task('install-assets', function () {
 
-	// Bootstrap Fonts
-	var fonts_twbs = gulp.src(
-		'node_modules/bootstrap-sass/assets/fonts/**/*',
-		{base: 'node_modules/bootstrap-sass/assets/fonts'})
-		.pipe(gulp.dest('src/fonts/vendor'));
+    // Bootstrap Fonts
+    var fonts_twbs = gulp.src(
+        'node_modules/bootstrap-sass/assets/fonts/**/*',
+        {base: 'node_modules/bootstrap-sass/assets/fonts'})
+        .pipe(gulp.dest('src/fonts/vendor'));
 
-	// JavaScripts
-	var js = gulp.src([
-		'node_modules/jquery/dist/jquery.js',
-		'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
-		'node_modules/enquire.js/dist/enquire.min.js'
-	]).pipe(gulp.dest('src/js/vendor'));
+    // JavaScripts
+    var js = gulp.src([
+        'node_modules/jquery/dist/jquery.js',
+        'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
+        'node_modules/enquire.js/dist/enquire.min.js'
+    ]).pipe(gulp.dest('src/js/vendor'));
 
-	return merge_streams(fonts_twbs, js);
+    return merge_streams(fonts_twbs, js);
 });
 
 
@@ -44,10 +44,9 @@ gulp.task('production-assets', function () {
         .pipe(gulp.dest('dist/fonts'));
 
     // Stylesheets and JavaScripts are handled by minifier
-
-	var images = gulp.src('src/img/**/*')
-		.pipe(imagemin())
-		.pipe(gulp.dest('dist/img'));
+    var images = gulp.src('src/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
 
     var videos = gulp.src('src/video/**/*')
         .pipe(gulp.dest('dist/video'));
@@ -56,7 +55,7 @@ gulp.task('production-assets', function () {
         .pipe(rename('.htaccess'))
         .pipe(gulp.dest('dist'));
 
-	return merge_streams(fonts, images, videos, htaccess);
+    return merge_streams(fonts, images, videos, htaccess);
 });
 
 /**
@@ -67,20 +66,20 @@ gulp.task('production-assets', function () {
  * @link https://github.com/dlmanning/gulp-sass
  */
 gulp.task('sass:prod', function () {
-	return gulp.src('scss/**/*.scss')
-		.pipe(
-			sass({
-				outputStyle: 'compressed',
-				precision: 8,
-				indentType: 'tab',
-				indentWidth: 1
-			}).on('error', sass.logError)
-		)
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
-		}))
-		.pipe(gulp.dest('dist/css/'));
+    return gulp.src('scss/**/*.scss')
+        .pipe(
+            sass({
+                outputStyle: 'compressed',
+                precision: 8,
+                indentType: 'tab',
+                indentWidth: 1
+            }).on('error', sass.logError)
+        )
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist/css/'));
 });
 
 
@@ -92,17 +91,17 @@ gulp.task('sass:prod', function () {
  * @link https://github.com/terinjokes/gulp-uglify
  */
 gulp.task('jsminify', function (cb) {
-	pump([
-			gulp.src('src/js/**/*'),
-			uglify({
-				output: {
-					comments: true
-				}
-			}),
-			gulp.dest('dist/js')
-		],
-		cb
-	);
+    pump([
+            gulp.src('src/js/**/*'),
+            uglify({
+                output: {
+                    comments: true
+                }
+            }),
+            gulp.dest('dist/js')
+        ],
+        cb
+    );
 });
 
 
@@ -115,16 +114,16 @@ gulp.task('jsminify', function (cb) {
  */
 gulp.task('htmlminify', () => {
     // Check if .shtml will work, too
-	return gulp.src('src/*.html')
-		.pipe(htmlmin({
-			minifyJS: true,
-			collapseWhitespace: true,
-			conservativeCollapse: true,
-			removeComments: true,
-			removeScriptTypeAttributes: true,
-			removeStyleLinkTypeAttributes: true
-		}))
-		.pipe(gulp.dest('dist'));
+    return gulp.src('src/*.html')
+        .pipe(htmlmin({
+            minifyJS: true,
+            collapseWhitespace: true,
+            conservativeCollapse: true,
+            removeComments: true,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true
+        }))
+        .pipe(gulp.dest('dist'));
 });
 
 
@@ -138,21 +137,21 @@ gulp.task('htmlminify', () => {
  * @link https://github.com/vohof/gulp-livereload
  */
 gulp.task('sass:dev', function () {
-	return gulp.src('scss/**/*.scss')
-		.pipe(sourcemaps.init())
-		.pipe(
-			sass({
-				outputStyle: 'expanded',
-				precision: 8,
-				indentType: 'tab',
-				indentWidth: 1
-			}).on('error', sass.logError)
-		)
-		.pipe(sourcemaps.write('sourcemaps'))
-		.pipe(gulp.dest('src/css'))
-		// make sure livereload will only reload the css and not the entire page when passing .map files in
-		.pipe(filter('**/*.css'))
-		.pipe(livereload());
+    return gulp.src('scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(
+            sass({
+                outputStyle: 'expanded',
+                precision: 8,
+                indentType: 'tab',
+                indentWidth: 1
+            }).on('error', sass.logError)
+        )
+        .pipe(sourcemaps.write('sourcemaps'))
+        .pipe(gulp.dest('src/css'))
+        // make sure livereload will only reload the css and not the entire page when passing .map files in
+        .pipe(filter('**/*.css'))
+        .pipe(livereload());
 });
 
 /**
@@ -163,17 +162,17 @@ gulp.task('sass:dev', function () {
  * @link https://github.com/vohof/gulp-livereload
  */
 gulp.task('dev', ['sass:dev'], function () {
-	livereload.listen();
-	gulp.watch('scss/**/*.scss', ['sass:dev']);
-	gulp.watch([
-		'src/**/*.html',
-		'src/**/*.shtml',
-		'src/**/*.php',
-		'src/js/**/*',
-		'src/img/**/*'
-	], function (event) {
-		livereload.changed(event.path);
-	});
+    livereload.listen();
+    gulp.watch('scss/**/*.scss', ['sass:dev']);
+    gulp.watch([
+        'src/**/*.html',
+        'src/**/*.shtml',
+        'src/**/*.php',
+        'src/js/**/*',
+        'src/img/**/*'
+    ], function (event) {
+        livereload.changed(event.path);
+    });
 });
 
 gulp.task('setup', ['install-assets', 'sass:dev']);
