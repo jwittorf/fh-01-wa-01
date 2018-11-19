a4ab54411639cded44a81d67f0a52633d8ba0050
Julian Wittorf <jwittorf@die-netzwerkstatt.de>
2018-11-19 17:59:10 +0100

    [DOC] Comment on what the AJAX request actually does

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,15 @@
 function loadDocument() {
-    // TODO check the exact purposes, so you know, what you're doing
+    // Explanation see here: https://www.w3schools.com/xml/ajax_xmlhttprequest_create.asp
+    // Create a new request object
     var xhttp = new XMLHttpRequest();
+    // Define what to do when the request changes
     xhttp.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
             document.getElementById("ajax-demo-content").innerHTML = this.responseText;
         }
     };
+    // Specify the request, don't do anything yet
     xhttp.open("GET", "ajax/hello-world.txt", true);
+    // Send the actual request
     xhttp.send();
 }
