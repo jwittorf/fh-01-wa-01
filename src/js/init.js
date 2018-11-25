d9ecb8f2b0129d17520dd45f9152ef4df776bc83
Julian Wittorf <jwittorf@die-netzwerkstatt.de>
2018-11-25 12:06:50 +0100

    [TASK] Create basic ajax demo, move initialization to separate file
    * Removing the onclick="" attribute from HTML makes the code more flexible by separating content from functionality
    * We use main.js to define our own functions
    * We call our functions in init.js

---
+++ src/js/init.js
@@ -0,0 +1,7 @@
+// Initialize/call the methods from main.js
+window.onload = function () {
+	// Load demo "Hello World" content via ajax
+    document.getElementById("ajax-demo-button").onclick = function() {
+        loadDocument("ajax/hello-world.txt", "ajax-demo-content");
+    }
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,18 @@
-function loadDocument() {
+// This file keeps all our functions, we call them in init.js
+
+// Load a document's content into a html element, specified by id
+function loadDocument(filename = "ajax/hello-world.txt", htmlId = "ajax-demo-content") {
     // Explanation see here: https://www.w3schools.com/xml/ajax_xmlhttprequest_create.asp
     // Create a new request object
     var xhttp = new XMLHttpRequest();
     // Define what to do when the request changes
     xhttp.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
-            document.getElementById("ajax-demo-content").innerHTML = this.responseText;
+            document.getElementById(htmlId).innerHTML = this.responseText;
         }
     };
     // Specify the request, don't do anything yet
-    xhttp.open("GET", "ajax/hello-world.txt", true);
+    xhttp.open("GET", filename, true);
     // Send the actual request
     xhttp.send();
 }
